5276aa5d37fa7b8a5f8f7a6e0186452c89c4d5b7
Pushpa latha <pushpalatha_ram@yahoo.co.in>
2020-05-11 16:20:33 -0700

    Basic SOAP Service sample

---
+++ SampleSOAP.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MathService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMathService" in both code and config file together.
+    [ServiceContract]
+    public interface IIdPasswordSOAP
+    {
+        [OperationContract]
+        string passwordCreate(string firstName, string lastName, int age);
+        [OperationContract]
+        int loginId(int age);
+
+    }
+}
---
+++ SampleSOAP.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+namespace MathService
+{
+    public class MathService : SampleSOAP
+    {
+        public string passwordCreate(string firstName, string lastName, int age)
+        {
+            return lastName.Substring(0, 2) + firstName.Substring(firstName.Length - 2) + age % 5;
+        }
+
+        public int loginId(int age)
+        {
+
+            return (age * new Random().Next(100, 200));
+        }
+    }
+}
